7299aef2b66e27d55461c7d99c887a84f379d6cb
hihailey <hihailey96@gmail.com>
2024-05-24 14:36:23 -0700

    feat: add calculation func

--- src/Components/Calculator.tsx
+++ src/Components/Calculator.tsx
@@ -1,9 +1,37 @@
-import React from 'react'
+import { useState } from 'react'
+import { evaluate } from 'mathjs';
+
+const buttons = ['C', '+/-', '%', '/', 7, 8, 9, '*', 4, 5, 6, '-', 1, 2, 3, '+','.', '0', '<','=']
+
 
 const Calculator = () => {
+    const [result, setResult] = useState('0');
+    
+  const onClickButton = (input: number|string) => {
+    console.log(input)
+    // If the result is '0', set the new input, otherwise append the new input
+    if (typeof input === 'number') {
+        if (result === '0') {
+            setResult(String(input));
+          } else {
+            setResult(result + input);
+          }
+    } else {
+        if (input === 'C') {
+            setResult('0');
+          } else if (input === '=') {
+            try {
+              const finalValue = evaluate(result);
+              setResult(finalValue.toString());
+            } catch (error) {
+                setResult('Error');
+            }
+          } else {
+            setResult(result + input);
+          }
 
-    const buttons = ['C', '+/-', '%', '/', 7, 8, 9, '*', 4, 5, 6, '-', 1, 2, 3, '+','.', '0', '<','=']
-
+    }
+  };
 
   return (
 
@@ -11,10 +39,10 @@ const Calculator = () => {
     <>
     <div className=''>
 
-    <p>Result</p>
+    <p>{result}</p>
     <div className='buttonGrid'>
         {buttons.map ((button, index) => {
-            return <button key={index}>{button}</button>
+            return <button onClick={()=> onClickButton(button)} key={index}>{button}</button>
         } )}
       </div>    
       
